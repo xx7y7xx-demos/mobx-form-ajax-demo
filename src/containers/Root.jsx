5374f26111bcd4feef42e316e1d01f57b385e665
Devin Chen <d3vin.chen@gmail.com>
2017-05-22 16:40:48 +0800

    add mobx-react-form example

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -3,8 +3,40 @@ import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 // import DevTools from 'mobx-react-devtools';
 
+import validatorjs from 'validatorjs';
+import MobxReactForm from 'mobx-react-form';
+
+const plugins = { dvr: validatorjs };
+
+const fields = [{
+  name: 'email',
+  label: 'Email',
+  placeholder: 'Insert Email',
+  rules: 'required|email|string|between:5,25',
+}];
+
+class MyForm extends MobxReactForm {
+
+  onSuccess(form) {
+    alert('Form is valid! Send the request here.');
+    // get field values
+    console.log('Form Values!', form.values());
+  }
+
+  onError(form) {
+    // get all form errors
+    console.log('All form errors', form.errors());
+    // invalidate the form with a custom error message
+    form.invalidate('This is a generic error message!');
+  }
+}
+
 @observer
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.mformInstance = new MyForm({ fields }, { plugins });
+  }
   onReset = () => {
     this.props.appState.resetTimer();
   }
@@ -14,6 +46,22 @@ class App extends Component {
         <button onClick={this.onReset}>
           Seconds passed: {this.props.appState.timer}
         </button>
+        <hr />
+        <form onSubmit={this.mformInstance.onSubmit}>
+          <label htmlFor={this.mformInstance.$('email').id}>
+            {this.mformInstance.$('email').label}
+          </label>
+          <input
+            {...this.mformInstance.$('email').bind()}
+          />
+          <p>{this.mformInstance.$('email').error}</p>
+
+          <button type="submit" onClick={this.mformInstance.onSubmit}>Submit</button>
+          <button type="button" onClick={this.mformInstance.onClear}>Clear</button>
+          <button type="button" onClick={this.mformInstance.onReset}>Reset</button>
+
+          <p>{this.mformInstance.error}</p>
+        </form>
         {/* <DevTools />*/}
       </div>
     );
